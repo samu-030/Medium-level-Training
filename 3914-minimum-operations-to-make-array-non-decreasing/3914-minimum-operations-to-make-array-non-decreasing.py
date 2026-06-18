a2d3af7bf551210a4badaf9150c1b29b988ccbe1
Samundishwari <samuprithi@gmail.com>
2026-06-18 10:46:24 +0530

    Time: 78 ms (52.28%), Space: 21.5 MB (50.6%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.py
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.py
@@ -4,10 +4,10 @@ class Solution(object):
         res = 0
         n = len(nums)
 
-        for i in range(1, n):
-            if nums[i] < nums[i-1]:
-                res += nums[i-1] - nums[i]
+        for i in range(n-1):
+
+            if nums[i] > nums[i+1]:
+                res += nums[i] - nums[i+1]
 
         return res
-
 
