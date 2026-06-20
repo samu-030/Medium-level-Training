edfbcddbb7c9f9218b54e884f403829379e247cc
Samundishwari <samuprithi@gmail.com>
2026-06-20 09:48:14 +0530

    Time: 15 ms (20.09%), Space: 15.9 MB (90.57%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def merge(self, intervals):
+        
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+
+        for interval in intervals:
+            
+            if merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
+        
