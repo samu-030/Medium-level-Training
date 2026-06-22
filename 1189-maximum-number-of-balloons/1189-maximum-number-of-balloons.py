f8d0a85dccffc91a5270c116c428b18732f1fd89
Samundishwari <samuprithi@gmail.com>
2026-06-22 12:31:37 +0530

    Time: 3 ms (61.87%), Space: 19.4 MB (38.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,7 +1,13 @@
+from collections import Counter
+
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
 
-        b = a = l = o = n = 0
+        counts = Counter(text)
+
+        return min(counts["b"], counts["a"], counts["l"]//2, counts["o"]//2, counts["n"])
+
+        """b = a = l = o = n = 0
 
         for c in text:
             match c:
@@ -16,4 +22,4 @@ class Solution:
                 case "n":
                     n += 1
 
-        return min(b, a, l//2, o//2, n)
+        return min(b, a, l//2, o//2, n)"""
