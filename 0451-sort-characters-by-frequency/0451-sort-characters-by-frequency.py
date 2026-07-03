92b7a4ca0ea74e52e3d578f707b579bd5fd50806
Samundishwari <samuprithi@gmail.com>
2026-07-03 10:13:42 +0530

    Time: 13 ms (87.8%), Space: 13.6 MB (29.44%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,33 +1,37 @@
 class Solution(object):
     def frequencySort(self, s):
-
         freq = {}
+        res = []
+
         for i in s:
             freq[i] = freq.get(i, 0) + 1
 
-        res = ""
+        freq_sorted = sorted(freq.items(), key = lambda x: x[1],  reverse = True)
 
-        while freq:
-            max_char = ""
-            max_cnt = 0
+        for i, cnt in freq_sorted:
+            res.append(i * cnt)
 
-            for char, count in freq.items():
-                if count > max_cnt:
-                    max_cnt = count
-                    max_char = char
-
-            res += max_char * max_cnt
-
-            del freq[max_char]
-
-        return res
-            
-"""freq = {}
-
-for i in s:
-    freq[i] = freq.get(i,0)+1
-
-freq_sorted = dict(sorted(freq.values()))
-
-return "".join(freq_sorted)"""
+        return "".join(res)
         
+
+"""freq = {}
+for i in s:
+    freq[i] = freq.get(i, 0) + 1
+
+res = ""
+
+while freq:
+    max_char = ""
+    max_cnt = 0
+
+    for char, count in freq.items():
+        if count > max_cnt:
+            max_cnt = count
+            max_char = char
+
+    res += max_char * max_cnt
+
+    del freq[max_char]
+
+return res"""
+            
