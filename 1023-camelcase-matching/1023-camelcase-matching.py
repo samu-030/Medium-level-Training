752a867b1a0b4a8a9ebeb6e3dcf6431b65cde260
Samundishwari <samuprithi@gmail.com>
2026-07-03 15:10:37 +0530

    Time: 1 ms (32.5%), Space: 12.4 MB (47.5%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution(object):
+    def camelMatch(self, queries, pattern):
+        def matches(query, pattern):
+            j = 0
+            for i in range(len(query)):
+                if j < len(pattern) and query[i] == pattern[j]:
+                    j += 1
+
+                elif query[i].isupper():
+                    return False
+            return j == len(pattern)
+
+        return [matches(query, pattern) for query in queries]
+        
+        
