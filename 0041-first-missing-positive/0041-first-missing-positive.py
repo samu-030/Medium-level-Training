886c468c87446281dc55645908bbe12321b37386
Samundishwari <samuprithi@gmail.com>
2026-07-15 14:40:19 +0530

    Time: 25 ms (88.88%), Space: 26.2 MB (9.13%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,18 +1,25 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
 
-        nums.sort()
-        min_val = 1
+        seen = set(nums)
+        for i in range(1, len(nums)+1):
+            if i not in seen:
+                return i
+                
+        return len(nums)+1
 
-        for i in nums:
-            if i < min_val: 
-                continue
-            elif i == min_val: 
-                min_val += 1
-            else: 
-                return min_val
+"""nums.sort()
+min_val = 1
 
+for i in nums:
+    if i < min_val: 
+        continue
+    elif i == min_val: 
+        min_val += 1
+    else: 
         return min_val
+
+return min_val"""
 
 
 
